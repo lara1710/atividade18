1987ad99c53d2cb0448f8ed7af11e2af70b972e8
lara1710 <laraleticia1203@gmail.com>
2024-09-18 15:46:33 -0300

    repetição

---
+++ atividade18.py
@@ -0,0 +1,7 @@
+# Crie um programa que utilize uma estrutura de repetição para imprimir os números de 1 a 10
+
+estrutura = [1,2,3,4,5,6,7,8,9,10]
+
+for estrutura in range (11):
+    print(estrutura)
+
